53cfd66d03a8a62020e6f63bb14f0ac507497f83
Roman Varga <varga@localhost.company>
2021-04-28 14:59:46 +0200

    readme update, structure update, IDG

--- ManualAugmentations.py
+++ ManualAugmentations.py
@@ -21,13 +21,11 @@ i = 0
 for image_name in os.listdir('datasetcovid/non-COVID/') :
     print(image_name)
     image = cv2.imread('datasetcovid/non-COVID/'+image_name)
-    # cv2.imwrite('manually_augmented/COVID_dataset/non-COVID/original_'+image_name, image)
     augmented_image = transform(image=image)
     cv2.imwrite('manually_augmented/COVID_dataset/non-COVID/'+image_name, augmented_image['image'])
     #visualize(augmented_image['image'])
 for image_name in os.listdir('datasetcovid/COVID/') :
     image = cv2.imread('datasetcovid/COVID/'+image_name)
-    # cv2.imwrite('manually_augmented/COVID_dataset/COVID/original_'+image_name, image)
     augmented_image = transform(image=image)
     cv2.imwrite('manually_augmented/COVID_dataset/COVID/'+image_name, augmented_image['image'])
     #visualize(augmented_image['image'])
@@ -35,13 +33,11 @@ for image_name in os.listdir('datasetcovid/COVID/') :
 for image_name in os.listdir('resizedMRI224x224/yes/') :
     print(image_name)
     image = cv2.imread('resizedMRI224x224/yes/'+image_name)
-    # cv2.imwrite('manually_augmented/MRI_dataset/yes/original_' + image_name, image)
     augmented_image = transform(image=image)
     cv2.imwrite('manually_augmented/MRI_dataset/yes/'+image_name, augmented_image['image'])
     #visualize(augmented_image['image'])
 for image_name in os.listdir('resizedMRI224x224/no/') :
     image = cv2.imread('resizedMRI224x224/no/'+image_name)
-    # cv2.imwrite('manually_augmented/MRI_dataset/no/original_'+image_name,image)
     augmented_image = transform(image=image)
     cv2.imwrite('manually_augmented/MRI_dataset/no/'+image_name, augmented_image['image'])
     #visualize(augmented_image['image'])
